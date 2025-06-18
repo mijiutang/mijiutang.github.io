40732c208ef25b810d11f5f6987018c81f450ab8
mijiutang <129970328+mijiutang@users.noreply.github.com>
2025-06-18 12:26:45 +0800

    del

--- src/core/util/env.js
+++
@@ -1,9 +0,0 @@
-const computedStyle = getComputedStyle(document.documentElement, null);
-
-export const mobileBreakpoint = computedStyle.getPropertyValue(
-  '--_mobile-breakpoint',
-);
-
-export function isMobile() {
-  return window?.matchMedia?.(`(max-width: ${mobileBreakpoint})`)?.matches;
-}
